372e524deac47388cd1ee331f6cdb72f277197b9
Pete Browne <me@petebrowne.com>
2010-08-25 10:48:22 -0500

    Adding javascript compression to Javascript Package

--- Gemfile
+++ Gemfile
@@ -6,4 +6,6 @@ group :development do
   gem 'rspec',          '~> 2.0.0.beta.20'
   gem 'activesupport',  '~> 3.0.0.rc2'
   gem 'test-construct', '~> 1.2.0', :require => 'construct'
+  gem 'jsmin',          '~> 1.0.1', :require => false
+  gem 'packr',          '~> 3.1.0', :require => false
 end
--- lib/rack/pack/packages/javascript.rb
+++ lib/rack/pack/packages/javascript.rb
@@ -2,7 +2,16 @@ module Rack
   module Pack
     module Packages
       class Javascript < Rack::Pack::Package
-        
+        def compile
+          compiled = super
+          if defined?(JSMin)
+            JSMin.minify(compiled).strip
+          elsif defined?(Packr)
+            Packr.pack(compiled, :shrink_vars => true).strip
+          else
+            compiled.strip
+          end
+        end
       end
     end
   end
